47901b24cfc0fc45309fec54fb12f0a6f930b9a6
Szymon Tracz <szymontracz1@gmail.com>
2022-10-04 10:44:04 +0200

    part of product tests done

--- src/main/java/com/codecool/shop/model/Product.java
+++ src/main/java/com/codecool/shop/model/Product.java
@@ -13,7 +13,7 @@ public class Product extends BaseModel {
 
     public Product(String name, BigDecimal defaultPrice, String currencyString, String description, ProductCategory productCategory, Supplier supplier) {
         super(name, description);
-        this.setPrice(defaultPrice, currencyString);
+        this.setDefaultPrice(defaultPrice);
         this.setSupplier(supplier);
         this.setProductCategory(productCategory);
     }
@@ -36,15 +36,6 @@ public class Product extends BaseModel {
 
     public String getPriceAsString() {
         return this.defaultPrice + " " + this.defaultCurrency.toString();
-    }
-
-    public BigDecimal getPrice() {
-        return this.defaultPrice;
-    }
-
-    public void setPrice(BigDecimal price, String currency) {
-        this.defaultPrice = price;
-        this.defaultCurrency = Currency.getInstance(currency);
     }
 
     public ProductCategory getProductCategory() {
---
+++ src/test/java/com/codecool/shop/model/ProductTest.java
@@ -0,0 +1,67 @@
+package com.codecool.shop.model;
+
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+class ProductTest {
+
+    @Test
+    public void test_getDefaultPrice() {
+        Supplier supplierMock = mock(Supplier.class);
+        ProductCategory productCategoryMock = mock(ProductCategory.class);
+        Product product = new Product("Name", new BigDecimal("7"), "USD", "Fantastic.", productCategoryMock, supplierMock);
+        BigDecimal defaultBig = new BigDecimal("7");
+        assertEquals(0, product.getDefaultPrice().compareTo(defaultBig));
+    }
+
+    @Test
+    public void test_setDefaultPrice() {
+        Supplier supplierMock = mock(Supplier.class);
+        ProductCategory productCategoryMock = mock(ProductCategory.class);
+        Product product = new Product("Name", new BigDecimal("3"), "USD", "Fantastic.", productCategoryMock, supplierMock);
+        BigDecimal newBig = new BigDecimal("7");
+        product.setDefaultPrice(newBig);
+        assertEquals(0, product.getDefaultPrice().compareTo(newBig));
+    }
+
+    @Test
+    public void test_getDefaultCurrency() {
+        Supplier supplierMock = mock(Supplier.class);
+        ProductCategory productCategoryMock = mock(ProductCategory.class);
+        Product product = new Product("Name", new BigDecimal("3"), "USD", "Fantastic.", productCategoryMock, supplierMock);
+        assertEquals(product.getDefaultCurrency().getCurrencyCode(), "USD");
+    }
+
+    @Test
+    public void test_setDefaultCurrency() {
+        Supplier supplierMock = mock(Supplier.class);
+        ProductCategory productCategoryMock = mock(ProductCategory.class);
+        Product product = new Product("Name", new BigDecimal("3"), "USD", "Fantastic.", productCategoryMock, supplierMock);
+        product.setDefaultCurrency(Currency.getInstance("EUR"));
+        assertEquals(product.getDefaultCurrency().getCurrencyCode(), "EUR");
+    }
+
+    @Test
+    public void test_getPriceAsString() {
+        Supplier supplierMock = mock(Supplier.class);
+        ProductCategory productCategoryMock = mock(ProductCategory.class);
+        Product product = new Product("Name", new BigDecimal("3"), "USD", "Fantastic.", productCategoryMock, supplierMock);
+        assertEquals(product.getPriceAsString(), "3 USD");
+    }
+
+
+
+
+
+
+
+}
