c2f2557a431b2bdfd056afee6769f5ba53234f8b
aganowak <agnieszka.nowak@gmx.com>
2022-10-04 15:33:06 +0200

    getAll method implemented

--- src/main/java/com/codecool/shop/dao/implementation/ProductDaoDB.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoDB.java
@@ -11,6 +11,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Currency;
 import java.util.List;
 
@@ -48,7 +49,7 @@ public class ProductDaoDB implements ProductDao {
                     ON p.category_id = p_c.id
             		LEFT JOIN suppliers AS s
                     ON p.supplier_id = s.id
-                    WHERE p.id = 1;
+                    WHERE p.id = ?;
             """;
             PreparedStatement st = conn.prepareStatement(sql);
             st.setInt(1, id);
@@ -83,12 +84,77 @@ public class ProductDaoDB implements ProductDao {
 
     @Override
     public List<Product> getAll() {
-        return null;
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = """
+            SELECT  p.name,
+                            p.price,
+                            p.currency,
+                            p.description,
+            				p_c.name,
+                            p_c.department,
+                            p_c.description,
+            				s.name,
+                            s.description 
+                    FROM products AS p
+            		LEFT JOIN product_categories AS p_c
+                    ON p.category_id = p_c.id
+            		LEFT JOIN suppliers AS s
+                    ON p.supplier_id = s.id
+            """;
+            ResultSet rs = conn.createStatement().executeQuery(sql);
+            List<Product> allProducts = new ArrayList<>();
+            while (rs.next()) { // while result set pointer is positioned before or on last row read authors
+                String name = rs.getString(1);
+                String description = rs.getString(2);
+                BigDecimal defaultPrice = rs.getBigDecimal(3);
+                String defaultCurrency = rs.getString(4);
+                ProductCategory productCategory = new ProductCategory(rs.getString(5), rs.getString(6), rs.getString(7));
+                Supplier supplier = new Supplier(rs.getString(8), rs.getString(9));
+                allProducts.add(new Product(name, defaultPrice, defaultCurrency, description, productCategory, supplier));
+            }
+            return allProducts;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while reading all authors", e);
+        }
     }
 
     @Override
     public List<Product> getBy(Supplier supplier) {
-        return null;
+        try (Connection conn = dataSource.getConnection()) {
+            int id = supplier.getId();
+            String sql = """
+            SELECT  p.name,
+                            p.price,
+                            p.currency,
+                            p.description,
+            				p_c.name,
+                            p_c.department,
+                            p_c.description,
+            				s.name,
+                            s.description 
+                    FROM products AS p
+            		LEFT JOIN product_categories AS p_c
+                    ON p.category_id = p_c.id
+            		LEFT JOIN suppliers AS s
+                    ON p.supplier_id = s.id
+                    WHERE s.id=?
+            """;
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setInt(1, id);
+            ResultSet rs = conn.createStatement().executeQuery(sql);
+            List<Product> allProducts = new ArrayList<>();
+            while (rs.next()) { // while result set pointer is positioned before or on last row read authors
+                String name = rs.getString(1);
+                String description = rs.getString(2);
+                BigDecimal defaultPrice = rs.getBigDecimal(3);
+                String defaultCurrency = rs.getString(4);
+                ProductCategory productCategory = new ProductCategory(rs.getString(5), rs.getString(6), rs.getString(7));
+                allProducts.add(new Product(name, defaultPrice, defaultCurrency, description, productCategory, supplier));
+            }
+            return allProducts;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while reading all authors", e);
+        }
     }
 
     @Override
