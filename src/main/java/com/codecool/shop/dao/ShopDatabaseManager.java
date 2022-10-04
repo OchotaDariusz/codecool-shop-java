9b678553f16140efd18e6f6b2d06c247f660e1eb
Sylwester <tomczyk.sylwester@wp.pl>
2022-10-04 11:21:14 +0200

    database connection

---
+++ src/main/java/com/codecool/shop/dao/ShopDatabaseManager.java
@@ -0,0 +1,33 @@
+package com.codecool.shop.dao;
+
+import org.postgresql.ds.PGSimpleDataSource;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+public class ShopDatabaseManager {
+
+    public void setup() throws SQLException {
+        DataSource dataSource = connect();
+//        playerDao = new PlayerDaoJdbc(dataSource);
+//        gameStateDao = new GameStateDaoJdbc(dataSource);
+//        itemDao = new ItemDaoJdbc(dataSource);
+    }
+
+    private DataSource connect() throws SQLException {
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        String dbName = System.getenv("DB_NAME");
+        String user = System.getenv("USER");
+        String password = System.getenv("PASSWORD");
+
+        dataSource.setDatabaseName(dbName);
+        dataSource.setUser(user);
+        dataSource.setPassword(password);
+
+        System.out.println("Trying to connect");
+        dataSource.getConnection().close();
+        System.out.println("Connection ok.");
+
+        return dataSource;
+    }
+}
