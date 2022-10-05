ad48e3b14b5a2333ea70c7121e145866671f34ba
Ochota Dariusz <ochota.dariusz@gmail.com>
2022-10-05 10:32:54 +0200

    Read app config properties at start

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -2,14 +2,15 @@ package com.codecool.shop.config;
 
 import com.codecool.shop.dao.*;
 import com.codecool.shop.dao.implementation.*;
-import com.codecool.shop.model.*;
-
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Properties;
 
 @WebListener
 public class Initializer implements ServletContextListener {
@@ -19,11 +20,20 @@ public class Initializer implements ServletContextListener {
     public static ProductCategoryDao productCategoryDataStore;
     public static SupplierDao supplierDataStore;
     public static UserDao userDataStore;
+    public static Properties appProps;
     public static ProductInCartDao cartDataStore;
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-            ShopDatabaseManager sdb = new ShopDatabaseManager();
+//        if (getAppDaoConfig().equals(AppDaoConfig.MEMORY.getConfigName())) {
+//            productDataStore = new ProductDaoMem();
+//            productCategoryDataStore = new ProductCategoryDaoMem();
+//            supplierDataStore = new SupplierDaoMem();
+//            orderDataStore = new OrderDaoMem();
+//            userDataStore = new UserDaoMem();
+//        }
+
+        ShopDatabaseManager sdb = new ShopDatabaseManager();
         DataSource connection;
         try {
             connection = sdb.getConnection();
@@ -38,7 +48,7 @@ public class Initializer implements ServletContextListener {
         //productCategoryDataStore = new ProductCategoryDaoMem();
         //supplierDataStore = new SupplierDaoMem();
         //orderDataStore = new OrderDaoMem();
-        userDataStore = new UserDaoMem();
+        userDataStore = new UserDaoJdbc(connection);
 
         /*
         Supplier amazon = new Supplier("Amazon", "Digital content and services");
@@ -79,9 +89,43 @@ public class Initializer implements ServletContextListener {
         productDataStore.add(new Product("Galaxy Z Flip4 I Bespoke Edition", new BigDecimal("1169.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", smartphone, samsung));
         */
         //setting up users
-        userDataStore.add(new User("Tomek", "password"));
+//        userDataStore.add(new User("Tomek", "password"));
 
-        //setting up test order
+//        //setting up test order
 //        orderDataStore.add(new Order(1));
+    }
+
+    private enum AppDaoConfig {
+        MEMORY("memory"),
+        JDBC("jdbc");
+
+        private final String daoConfig;
+
+        AppDaoConfig(String daoConfig) {
+            this.daoConfig = daoConfig;
+        }
+
+        public String getConfigName() {
+            return daoConfig;
+        }
+    }
+
+    private String getAppDaoConfig() {
+        String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+        String appConfigPath = rootPath + "connection.properties";
+
+        appProps = new Properties();
+        try {
+            appProps.load(new FileInputStream(appConfigPath));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+//        String appDbUrl = appProps.getProperty("url");
+//        String appDbName = appProps.getProperty("database");
+//        String appDbUser = appProps.getProperty("user");
+//        String appDbPass = appProps.getProperty("password");
+        return appProps.getProperty("dao");
     }
 }
